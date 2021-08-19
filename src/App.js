7273e52eb8f109a12322d3791a6bc996e77345ad
ovenbakedgoods <ovenbakedgoods88@gmail.com>
2021-08-18 22:55:11 -0500

    Finishing MVP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import React from 'react';
-
+import { useReducer } from 'react';
 import './App.css';
-
+import reducer, { initialState} from './reducers';
+import { addOne } from './actions';
+import { applyNumber, changeOperation, clearDisplay, memoryPlus, memoryValue, memoryClear } from './actions';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
-function App() {
+const App = () =>{
+  const [state, dispatch ] = useReducer(reducer, initialState);
 
+  /*const add = () =>
+  {
+    dispatch(addOne());
+  }*/
+  const memoryplus = () =>
+  {
+    console.log("Memory Plus is working",state.memory);
+    dispatch(memoryPlus())
+  }
+  const add = (value) => {
+    dispatch(applyNumber(parseInt(value.target.textContent)))
+    //console.log(value.target.textContent);
+  }
+
+  const operate = (value) => 
+  {//console.log(value.target.textContent)
+    dispatch(changeOperation(value.target.textContent));
+  }
+
+  const clear = () =>
+  {
+    dispatch(clearDisplay());
+  }
+
+  const mrValue = (x) =>
+  {
+    console.log("Memory Value is working", state.memory);
+    dispatch(memoryValue);
+  }
+
+  const mrClear = () =>
+  {console.log("Value in memory is ", state.memory)
+    dispatch(memoryClear)
+  }
   return (
     <div className="App">
-      <nav className="navbar navbar-dark bg-dark">
+      <nav className="navbar navbar-dark bg-dark">value.target.textContent
         <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
       </nav>
 
@@ -17,44 +54,44 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> state.operation</span>
+              <span id="memory"><b>Memory:</b> state.memory</span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton value={"M+"} onClick={memoryplus}/>
+              <CalcButton value={"MR"} onClick={mrValue}/>
+              <CalcButton value={"MC"} onClick={mrClear}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={add}/>
+              <CalcButton value={2} onClick={add}/>
+              <CalcButton value={3} onClick={add}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={add}/>
+              <CalcButton value={5} onClick={add}/>
+              <CalcButton value={6} onClick={add}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={add}/>
+              <CalcButton value={8} onClick={add}/>
+              <CalcButton value={9} onClick={add}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={"+"} onClick={operate}/>
+              <CalcButton value={"*"} onClick={operate}/>
+              <CalcButton value={"-"} onClick={operate}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={clear}/>
             </div>
 
           </form>
